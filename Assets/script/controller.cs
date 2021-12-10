92604699651648318ed756e34b1e7f9d9990e4dd
aballantyne <andrewaballantyne@icloud.com>
2021-12-10 07:59:46 -0800

    Trying to make zoom
    doesn't work yet

--- Assets/script/controller.cs
+++ Assets/script/controller.cs
@@ -6,43 +6,44 @@ using System;
 
 public class controller : MonoBehaviour
 {
-    string input = "y=4*x+1";
+    string input = "y=1/9*x";
 
-    Vector2 postion = new Vector2(0,0); 
-    float zoom = 10; 
+    public Vector2 postion = new Vector2(0,0); 
+    public float zoom = 5; 
 
     LineRenderer lineRenderer;
     // Start is called before the first frame update
     void Start()
     {
-
+        lineRenderer = GetComponent<LineRenderer>();
+        function();
     }
 
     // Update is called once per frame
     void Update( )
     {
-        if (Input.mouseScrollDelta.y != 0){
-            zoom +=Input.mouseScrollDelta.y; 
-            Camera.main.orthographicSize= zoom;  
+        if (Input.mouseScrollDelta.y < 0 && zoom >= 0 || Input.mouseScrollDelta.y > 0){
+            zoom -=Input.mouseScrollDelta.y; 
             function();
         }
+        if (zoom < 0) zoom = 0;
     }
-    static Double Eval(String expression)
+    static float Eval(String expression)
     {
         System.Data.DataTable table = new System.Data.DataTable();
-        return Convert.ToDouble(table.Compute(expression, String.Empty));
+        return Convert.ToSingle(table.Compute(expression, String.Empty));
     }
     void function(){
         string[] side = input.Split('=');
         string str = side [1];
 
-        lineRenderer = GetComponent<LineRenderer>();
-        Vector3[] points = new Vector3[zoom];
-        int index = 0;
-     
-        for (float x = postion.x-zoom; x < postion.x + 10; x++){
-            points[index] = new Vector3(x,Convert.ToSingle(Eval(str.Replace("x",  Convert.ToString(x)))));
-            index++;
+        Vector3[] points = new Vector3[32]; 
+
+        float x = postion.x - zoom/2; 
+
+        for (int i = 0; i < 32; i++){
+            points[i] = new Vector3(x * zoom,Eval(str.Replace("x",  Convert.ToString(x)))*zoom);
+            x+= Math.Abs(zoom/32f);
         }
         lineRenderer.positionCount = points.Length;
         lineRenderer.SetPositions(points);
