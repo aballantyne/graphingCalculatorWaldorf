d02b56036b9a94e684db2c9322e88540c10f6902
aballantyne <andrewaballantyne@icloud.com>
2021-12-12 17:11:49 -0800

    Fixed some organization and tryed to fix the line

--- Assets/script/controller.cs
+++ Assets/script/controller.cs
@@ -6,47 +6,54 @@ using System;
 
 public class controller : MonoBehaviour
 {
-    string input = "y=1/9*x";
+    const int TOTAL_POINTS = 64; 
+    string input = "y=x";
 
-    public Vector2 postion = new Vector2(0,0); 
-    public float zoom = 5; 
+    public float distance = 20; 
 
     LineRenderer lineRenderer;
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        function();
+        RenderLine();
     }
 
     // Update is called once per frame
     void Update( )
     {
-        if (Input.mouseScrollDelta.y < 0 && zoom >= 0 || Input.mouseScrollDelta.y > 0){
-            zoom -=Input.mouseScrollDelta.y; 
-            function();
+        if (Input.mouseScrollDelta.y != 0 ){
+            distance += Input.mouseScrollDelta.y; 
+            if (distance<0)distance = 0.1f;
+            RenderLine();
         }
-        if (zoom < 0) zoom = 0;
     }
     static float Eval(String expression)
     {
         System.Data.DataTable table = new System.Data.DataTable();
         return Convert.ToSingle(table.Compute(expression, String.Empty));
     }
-    void function(){
-        string[] side = input.Split('=');
-        string str = side [1];
+    void RenderLine(){
 
-        Vector3[] points = new Vector3[32]; 
+        lineRenderer.positionCount = TOTAL_POINTS;
+        lineRenderer.SetPositions(CalculateLine());
 
-        float x = postion.x - zoom/2; 
+    }
+    Vector3[] CalculateLine(){
 
-        for (int i = 0; i < 32; i++){
-            points[i] = new Vector3(x * zoom,Eval(str.Replace("x",  Convert.ToString(x)))*zoom);
-            x+= Math.Abs(zoom/32f);
+        string str = input.Split('=')[1];
+
+        Vector3[] points = new Vector3[TOTAL_POINTS]; 
+
+        float x = -distance/2f; 
+        float increment = distance/TOTAL_POINTS;
+
+        for (int i = 0; i < TOTAL_POINTS; i++){
+            float newX = i/3.2f - 10 ;
+            float newY = Eval(str.Replace("x",  Convert.ToString(x))); 
+            points[i] = new Vector3(newX,newY);
+            x+= increment;
         }
-        lineRenderer.positionCount = points.Length;
-        lineRenderer.SetPositions(points);
-
+        return points;
     }
 }
